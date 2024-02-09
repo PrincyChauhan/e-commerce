44090cff9debcd24d32b8cbd2892c1ef2ce7bacd
PrincyChauhan <chauhanprincee7@gmail.com>
2024-02-09 14:09:13 +0530

    Add Popular page and fix css issue

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
         </div>
         <div className="hero-latest-btn">
           <div>Latest Collection</div>
-          <img scr={arrow_icon} alt="" />
+          <img src={arrow_icon} alt="" />
         </div>
       </div>
       <div className="hero-right">
