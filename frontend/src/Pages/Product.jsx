8211eb36e165f9b8c5827f4192d00746f7082aa0
PrincyChauhan <chauhanprincee7@gmail.com>
2024-02-12 16:02:12 +0530

    Descriptionbox added and typo solve

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,14 +1,17 @@
-import React, { useContext,useParmas } from "react";
+import React, { useContext,useParams } from "react";
 import Breadcrum from "../Components/Breadcrum/Breadcrum";
 import { ShopContext } from "../Context/ShopContext";
-
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
 const Product = () => {
   const { all_product } = useContext(ShopContext);
-  const { productId } = useParmas();
+  const { productId } = useParams();
   const product = all_product.find((e) => e.id === Number(productId));
   return(
     <div>
       <Breadcrum product={product} />
+      <ProductDisplay product={product}/>
+      <DescriptionBox/>
     </div>
   )
 };
