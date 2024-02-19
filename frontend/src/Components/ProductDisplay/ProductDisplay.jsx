fef3688a8011c4e04ca9e87a85189687143654f1
PrincyChauhan <chauhanprincee7@gmail.com>
2024-02-19 22:23:05 +0530

    Product display page update

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -5,7 +5,6 @@ import star_dull_icon from "../Assets/star_dull_icon.png";
 
 const ProductDisplay = (props) => {
   const { product } = props;
-  console.log(product)
 
   return (
     <div className="productdisplay">
@@ -29,36 +28,34 @@ const ProductDisplay = (props) => {
           <img src={star_icon} alt="" />
           <img src={star_dull_icon} alt="" />
           <p>122</p>
-          <div className="productdisplay-right-prices">
-            <div className="productdisplay-right-price-old">
-              ${product.old_price}
-            </div>
-            <div className="productdisplay-right-price-new">
-              ${product.new_price}
-            </div>
-            <div className="productdisplay-right-description">
-              A light weight, usually cotton, fabric with a small, all-over,
-              printed design. The print is commonly a floral pattern, but can be
-              of any design or motif.
-            </div>
-            <div className="productdisplay-right-size">
-              <h1>Select Size</h1>
-              <div className="productdisplay-right-sizes">
-                <div>S</div>
-                <div>M</div>
-                <div>L</div>
-                <div>XL</div>
-                <div>XLL</div>
-              </div>
-            </div>
-            <button>Add to Cart</button>
-            <p className="productdisplay-right-category">
-              <span>Category:</span> Women,T-shirt,Crop Top{" "}
-            </p>
-            <p className="productdisplay-right-category">
-              <span>Tags:</span> Morden, Latest{" "}
-            </p>
+        </div>
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">
+            ${product.old_price}
           </div>
+          <div className="productdisplay-right-price-new">
+            ${product.new_price}
+          </div>
+        </div>
+        <div className="productdisplay-right-description">
+          A light weight, usually cotton, fabric with a small, all-over,
+          printed design. The print is commonly a floral pattern, but can be
+          of any design or motif.
+        </div>
+        <div className="productdisplay-right-size">
+          <h1>Select Size</h1>
+          <div className="productdisplay-right-sizes">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button>Add to Cart</button>
+        <div className="productdisplay-right-category">
+          <p><span>Category:</span> Women,T-shirt,Crop Top</p>
+          <p><span>Tags:</span> Morden, Latest</p>
         </div>
       </div>
     </div>
